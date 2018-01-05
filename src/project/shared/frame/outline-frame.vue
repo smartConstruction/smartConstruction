ef5c0beed5da4f15d746b4f38ac2620a3790223a
Zhangxiaoquan <zxq8984@163.com>
2018-01-05 10:35:51 +0800

    增加页面autoscale的能力

--- src/project/shared/frame/outline-frame.vue
+++ src/project/shared/frame/outline-frame.vue
@@ -4,11 +4,10 @@
   </div>
 </template>
 <script>
-export default {
-}
+import './autoScale'
+export default {}
 </script>
 <style lang="scss">
-
 @mixin full-size {
   position: absolute;
   left: 0;
@@ -16,8 +15,16 @@ export default {
   top: 0;
   bottom: 0;
 }
+body {
+  margin: 0
+}
 #frame {
-  @include full-size;
+  width: 1366px;
+  height: 768px;
+  transform-origin: 0 0;
+  -ms-transform-origin: 0 0;
+  position: absolute;
+  top: 0;
   #outline-frame {
     @include full-size;
   }
