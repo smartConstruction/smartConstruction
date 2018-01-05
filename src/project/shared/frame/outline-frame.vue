040ed4a2bc0e86dea5c3fe9e5379b9cb9428ad41
Zhangxiaoquan <zxq8984@163.com>
2018-01-05 12:13:12 +0800

    添加公共样式

--- src/project/shared/frame/outline-frame.vue
+++ src/project/shared/frame/outline-frame.vue
@@ -7,29 +7,6 @@
 import './autoScale'
 export default {}
 </script>
-<style lang="scss">
-@mixin full-size {
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-}
-body {
-  margin: 0
-}
-#frame {
-  width: 1366px;
-  height: 768px;
-  transform-origin: 0 0;
-  -ms-transform-origin: 0 0;
-  position: absolute;
-  top: 0;
-  #outline-frame {
-    @include full-size;
-  }
-  #body-frame {
-    @include full-size;
-  }
-}
+<style>
+
 </style>
